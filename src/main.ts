0ac5b427e8776a23eb615cc962ed2c300b4990dc
SamEagle <samadeagle@yahoo.com>
2025-11-15 20:49:16 +0330

    Main - Add swagger and validationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,58 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // ----------------------
+  // Global pipes
+  // ----------------------
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip unknown properties
+      forbidNonWhitelisted: true, // Throw error if unknown properties exist
+      transform: true, // Auto-transform payloads to DTO instances
+    }),
+  );
+
+  // TODO:
+  // ----------------------
+  // Global exception filter
+  // ----------------------
+  // const { httpAdapter } = app.get(HttpAdapterHost);
+  // app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  // ----------------------
+  // Enable CORS
+  // ----------------------
+  // app.enableCors();
+
+  // ----------------------
+  // Swagger setup
+  // ----------------------
+  const config = new DocumentBuilder()
+    .setTitle('Aitka API')
+    .setDescription('Aitka NestJS backend API')
+    .setVersion('1.0')
+    // .addBearerAuth() // Optional JWT auth
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  // ----------------------
+  // Start server
+  // ----------------------
+  const port = Number(process.env.APP_PORT ?? 3000);
+  await app.listen(port);
+  Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
+  Logger.log(
+    `Swagger docs available on http://localhost:${port}/api-docs`,
+    'Bootstrap',
+  );
 }
-// FIXME:
-/* eslint-disable @typescript-eslint/no-floating-promises */
+
 bootstrap();
