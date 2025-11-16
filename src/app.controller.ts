ddc2b5410c32563941d80672cf35b73d60f44f78
SamEagle <samadeagle@yahoo.com>
2025-11-16 20:40:22 +0330

    Hello World - @All

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, All, Req } from '@nestjs/common';
+import { Request } from 'express';
+
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @All('hello')
+  handleHello(@Req() request: Request): string {
     return this.appService.getHello();
   }
 }
