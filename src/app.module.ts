52502447bf1e931aa8f9aa481d47417898f64556
SamEagle <samadeagle@yahoo.com>
2025-11-18 00:51:11 +0330

    PrismaModule - Init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { DatabaseModule } from './database/database.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [
-    // DatabaseModule
-  ],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
