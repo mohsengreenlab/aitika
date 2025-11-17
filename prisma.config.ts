4260e445efca01d933cccb89b856d009399d4c5b
SamEagle <samadeagle@yahoo.com>
2025-11-18 00:35:55 +0330

    DATABASE - Initial migration

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig, env } from "prisma/config";
+import { defineConfig, env } from 'prisma/config';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+// Load root .env manually
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: 'prisma/schema.prisma',
   migrations: {
-    path: "prisma/migrations",
+    path: 'prisma/migrations',
   },
-  engine: "classic",
+  engine: 'classic',
   datasource: {
-    url: env("DATABASE_URL"),
+    url: env('DATABASE_URL'),
   },
 });
